269d7d80ae78d1b1bb6a6f9d80c92300c4f6a4b7
gkr <krgk1990@gmail.com>
2021-06-01 10:08:55 +0530

    Photowall Code updated

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Photo from  './Photo';
+import PropTypes from 'prop-types';
+
+function PhotoWall(props){
+    return <div>
+             <a className="add-icon" onClick={props.onNavigate} href="#AddPhoto"></a>
+            {/*<button onClick={props.onNavigate} className="add-icon">  </button>*/}
+            <div className="photo-grid">
+                {props.posts.map((post,index) => <Photo key={index} post={post} 
+                                                        onRemovePhoto={props.onRemovePhoto}/>)}
+            </div>
+        </div>
+
+}
+
+PhotoWall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto:PropTypes.func.isRequired
+}
+
+export default PhotoWall;
